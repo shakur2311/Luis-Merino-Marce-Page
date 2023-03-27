717e1fb2d79196c2a0f212da4e32cc01870474a2
Shakur <luismerinomarce@gmail.com>
2023-03-26 19:47:48 -0500

    mi segundo commit

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ let skillsCheckBox = document.getElementById("skillsOption");
 let portfolioCheckBox = document.getElementById("portfolioOption");
 let contactCheckBox = document.getElementById("contactOption");
 let whoiamText = document.getElementById('whoiam-text');
+let mailSentModal = document.getElementById('mailSentModal');
+let mailSentModalContainer = document.getElementById('mailSentModalContainer');
 let contador = 1;
 let apiKEY= "4faf50bb0cb9f434d2384430af1c4414";
 let secretKEY = "4497b575058dd955db57992268de4f65";
 const whoiamTextsArray = ['FRONTEND DEVELOPER','BACKEND DEVELOPER','SYSTEMS ENGINEER'];
+
+
 
 const handleCheckBox = (event)=>{
     if(event.target.id=="aboutOption"){
@@ -58,22 +62,36 @@ const textWhoiamChanger = ()=>{
     },6000)
 }
 
-const emailSend = (from,subject,message)=>{
-    const data = JSON.stringify({
-        Messages: [{
-            From: {Email: "luis23231111@gmail.com"},
-            To: [{Email: "luismerinomarce@gmail.com", Name: "Luis Merino"}],
-            Subject: "hola",
-            TextPart: "hola2"
-        }]
-    })
-    fetch("https://api.mailjet.com/v3.1/send", {
-        method: "POST",
-        headers: {
-        'Content-Type':"Application/json",
-        'Authorization':`Bearer ${apiKEY}:${secretKEY}` },
-        body: data
-    }).then(res=>res.json).then(data=>console.log(data));
+const emailSend = ()=>{
+    try {
+        let nameInput =  document.getElementById("nameInput");
+        let subjectInput = document.getElementById("subjectInput");
+        let messageInput = document.getElementById("messageInput");
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "shakuriwo23@gmail.com",
+            Password : "04C76973FE42ACC6F1B4433BBD1245024960",
+            To : "luismerinomarce@gmail.com",
+            From : "shakuriwo23@gmail.com",
+            Subject : subjectInput.value,
+            Body : `Correo enviado por: ${nameInput.value} \n Mensaje: ${messageInput.value}`
+        }).then(      
+            ()=>{
+                mailSentModalContainer.style.opacity = 1;
+                mailSentModalContainer.style.visibility = "visible";
+                nameInput.value = "";
+                subjectInput.value = "";
+                messageInput.value = "";
+            }
+        );
+    } catch (error) {
+        window.alert("Error al enviar!");
+    }
 }
-emailSend();
+
+const closeModal = ()=>{
+    mailSentModalContainer.style.opacity = 0;
+    mailSentModalContainer.style.visibility = "hidden";
+}
+
 textWhoiamChanger();
